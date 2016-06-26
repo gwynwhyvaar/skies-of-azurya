21b598b556c347066e8cb6dd54a862411b562149
Ayotunde Ayoko <tunde.ayoko@hotmail.com>
2016-06-26 11:41:53 +0100

    refractored some old XNA classes to Monogame. it compiles too...loool

--- SkiesOfAzurya_Dx/SkiesOfAzurya_Dx/Program.cs
+++ SkiesOfAzurya_Dx/SkiesOfAzurya_Dx/Program.cs
@@ -14,7 +14,7 @@ namespace SkiesOfAzurya_Dx
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new SkiesOfAzurya.SkiesOfAzuryaGame())
                 game.Run();
         }
     }
